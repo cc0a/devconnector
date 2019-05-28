06379c61a2e97ff957bf76679525c6b37a40cd04
cc0a <jmckeeco84@gmail.com>
2019-05-27 22:57:23 -0400

    account actions

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,25 +3,20 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
+import { IoIosLogOut, IoIosPerson } from 'react-icons/io';
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li>
-                <Link to='/profiles'>Developers</Link>
-            </li>
-            <li>
-                <Link to='/posts'>Posts</Link>
-            </li>
-            <li>
                 <Link to='/dashboard'>
-                    <i className='fas fa-user' />{' '}
+                    <IoIosPerson  />{' '}
                     <span className='hide-sm'>Dashboard</span>
                 </Link>
             </li>
             <li>
                 <a onClick={logout} href='#!'>
-                    <i className='fas fa-sign-out-alt' />{' '}
+                    <IoIosLogOut  />{' '}
                     <span className='hide-sm'>Logout</span>
                 </a>
             </li>
